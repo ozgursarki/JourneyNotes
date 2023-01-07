2a31a43ac445c0b0c53c728bf5f65b4c1b77b364
Sahan Sarki <sahan.sarki@nesine.com>
2023-01-07 16:34:01 +0300

    Marker Added

--- app/src/main/java/com/example/journeynotes/ui/MapsFragment.kt
+++ app/src/main/java/com/example/journeynotes/ui/MapsFragment.kt
@@ -1,23 +1,39 @@
 package com.example.journeynotes.ui
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
 import com.example.journeynotes.R
 import com.example.journeynotes.databinding.FragmentMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.skydoves.balloon.*
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener,OnMapClickListener {
 
     private lateinit var binding: FragmentMapsBinding
     private lateinit var mMap: GoogleMap
+
+    private var requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        if(it) {
+            Toast.makeText(requireContext(),"Permission Not Denied",Toast.LENGTH_LONG).show()
+        }else {
+            Toast.makeText(requireContext(),"Permission Denied",Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,15 +48,48 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
         val mapFragment = binding.map.getFragment<SupportMapFragment>()
         mapFragment.getMapAsync(this)
+       requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setOnMapClickListener(this)
+        mMap.setOnMarkerClickListener(this)
     }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+
+        val balloon = Balloon.Builder(requireContext())
+            .setWidthRatio(1.0f)
+            .setHeight(BalloonSizeSpec.WRAP)
+            .setText(getString(R.string.balloon_title))
+            .setTextSize(15f)
+            .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+            .setArrowSize(10)
+            .setArrowPosition(0.5f)
+            .setPadding(12)
+            .setCornerRadius(8f)
+            .setBalloonAnimation(BalloonAnimation.ELASTIC)
+            .setLifecycleOwner(viewLifecycleOwner)
+            .setOnBalloonClickListener {
+                val action = MapsFragmentDirections.actionMapsFragmentToAddNotesFragment()
+                findNavController().navigate(action)
+            }
+            .build()
+
+        binding.root.showAlignTop(balloon)
+
+        return true
+    }
+
+    override fun onMapClick(location: LatLng) {
+        mMap.clear()
+
+        mMap.addMarker(MarkerOptions().position(location).title("You clicked here!"))
+    }
+
+
 
 
 }
