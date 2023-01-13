20e0b3722ba0a526e3877b9a7f7ed74b97ec97db
ozgursarki <ozgurdogansarki@gmail.com>
2023-01-13 21:03:03 +0300

    AddNoteScreen Added

--- app/src/main/java/com/example/journeynotes/MainActivity.kt
+++ app/src/main/java/com/example/journeynotes/MainActivity.kt
@@ -2,17 +2,10 @@ package com.example.journeynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.journeynotes.databinding.ActivityMainBinding
-import com.example.journeynotes.ui.AddNotesFragment
-import com.example.journeynotes.ui.MapsFragment
-
-import com.google.android.gms.maps.GoogleMap
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,31 +17,12 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(MapsFragment())
+
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.mapsFragment -> replaceFragment(MapsFragment())
-                R.id.addNotesFragment -> replaceFragment(AddNotesFragment())
-
-                else -> {
-
-                }
-            }
-            true
-        }
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
-
-    private fun replaceFragment(fragment : Fragment) {
-         val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-         fragmentTransaction.replace(R.id.nav_host_fragment,fragment)
-        fragmentTransaction.commit()
-    }
-
-
 
 }
